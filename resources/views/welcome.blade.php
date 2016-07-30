a13286c5f05d5c1677ca3143bdfbd9f2063fc7a7
Anam hossain <anam@hotmail.com.au>
2016-07-30 22:40:02 +1000

    Show server info

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,6 +39,7 @@
         <div class="container">
             <div class="content">
                 <div class="title">Laravel 5</div>
+                <p>Server: <strong><?php echo php_uname();?></strong></p>
             </div>
         </div>
     </body>
